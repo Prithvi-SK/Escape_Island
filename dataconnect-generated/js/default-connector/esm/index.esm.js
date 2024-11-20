d5df367021334b8df2db0fbe419c078068eb3fa6
Prithvi-SK <prithvi.sk.0509@gmail.com>
2024-11-20 16:19:59 +0530

    try1

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,8 +0,0 @@
-import { getDataConnect, validateArgs } from 'firebase/data-connect';
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'vite-project',
-  location: 'us-central1'
-};
-
